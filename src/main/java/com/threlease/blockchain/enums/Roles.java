21b1d300f9209d23c50935484a9f05d463e882cf
th-release <cth.release@gmail.com>
2023-12-20 09:09:41 +0900

    feat: Added Enums

---
+++ src/main/java/com/threlease/blockchain/enums/Roles.java
@@ -0,0 +1,10 @@
+package com.threlease.blockchain.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Roles {
+    ROLE_USER, ROLE_ADMIN
+}
