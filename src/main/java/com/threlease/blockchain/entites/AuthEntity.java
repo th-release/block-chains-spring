8189cfd052fd2fef775cf9f2116b1056ef27b21b
th-release <cth.release@gmail.com>
2023-12-20 09:09:32 +0900

    feat: Added Entites

---
+++ src/main/java/com/threlease/blockchain/entites/AuthEntity.java
@@ -0,0 +1,40 @@
+package com.threlease.blockchain.entites;
+
+import com.threlease.blockchain.enums.Roles;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Entity
+@Getter
+@Setter
+@Table(name = "AuthEntity")
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class AuthEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "uuid", length = 36, columnDefinition = "varchar(36)", nullable = false)
+    private String uuid;
+
+    @Column(name = "username", columnDefinition = "varchar(24)", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", columnDefinition = "text", nullable = false)
+    private String password;
+
+    @Column(name = "salt", columnDefinition = "varchar(32)", length = 32, nullable = false)
+    private String salt;
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false, name = "createdAt")
+    private LocalDateTime createdAt;
+
+    @Enumerated(EnumType.STRING)
+    private Roles role;
+}
